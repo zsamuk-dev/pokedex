22ae630e8922877e776b90005b155ebb0208e5d0
zsamuk-dev <121689879+zsamuk-dev@users.noreply.github.com>
2023-01-04 21:27:41 -0300

    Rascunho 001

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+export default function App() {
+    return (
+        <div>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif" alt="pokemon" className="pokemon__image"/>
+
+            <h1 className="pokemon__data">
+                <span className="pokemon__number">6</span> - 
+                <span className="pokemon__name"> charizard</span>
+            </h1>
+
+            <form className="form">
+                <input type="search" 
+                className="input__search"
+                placeholder="Name or Number"
+                required
+                />
+            </form>
+
+            <div className="buttons">
+            <button className="button btn-prev">&lt; Prev</button>
+            <button className="button btn-next">Next &gt;</button>
+            </div>
+        </div>
+    )
+}
